080dc790b2b762df30728b23cccb3994625fd3b2
Michael Xiang <yorkmike4@gmail.com>
2021-02-19 08:49:13 +0000

    merging

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -1,17 +1,21 @@
-import React from "react";
-import useApplicationData from "./useApplicationData";
-import "./App.css";
-import Search from "./components/search.js";
-import OppositeTimeline from "./components/OppositeTimeline.js";
-import { userData, repoData } from "./backupData";
-import Filter from "./components/Filter";
-import NavBar from "./components/AppBar.js";
-import { useState } from "react";
-import ShowLiked from "./components/showLiked";
-import Scroll from "./components/scroll.js";
+import React  from 'react';
+import useApplicationData from './useApplicationData';
+import './App.css';
+import Search from './components/search.js'
+import OppositeTimeline from './components/OppositeTimeline.js'
+import {userData, repoData} from "./backupData"
+import Filter from "./components/Filter"
+import NavBar from "./components/AppBar.js"
+import { useState } from 'react'
+import ShowLiked from './components/showLiked';
+
+
+
 
 export default function Application(props) {
-  const { setStorage, setUser, fetchData, state } = useApplicationData();
+
+  const { setStorage, setUser, fetchData, state} = useApplicationData();
+  
 
   // const appointmentsObject = dailyAppointments.map(appointment => {
   //   const interview = getInterview(state, appointment.interview);
@@ -31,55 +35,48 @@ export default function Application(props) {
   //   );
   // })
 
-  const Show = () => {
-    return (
-      <div id="show">
-        <img src={state.avatar} alt="nothing"></img>
-        <a href={`https://github.com/${state.loginUser}`}>
-          <h4>@{state.loginUser}</h4>
-        </a>
-        <h4>{state.name}'s Timeline</h4>
-        <div id="opposite-timeline">
-          <OppositeTimeline
-            filterParam={filterParam}
-            repositories={state.repositories}
-            avatar={state.avatar}
-          />
-        </div>
-      </div>
-    );
-  };
-  const [filterParam, setParam] = useState();
-  const setFilter = (param) => {
-    setParam((prev) => param);
-  };
-  const [show, setShow] = useState("main");
-  const toLiked = () => {
-    setShow("liked");
-  };
-  const toMain = () => {
-    setShow("main");
-  };
+    const Show = () => {
+      return (<div id="show">
+                <img src={ state.avatar } alt="nothing"></img>
+                <a href={`https://github.com/${state.loginUser}`}>
+                  <h4>@{state.loginUser}</h4>
+                </a>
+                <h4>{state.name}'s Timeline</h4>
+                <div id="opposite-timeline">
+                  <OppositeTimeline filterParam={filterParam} repositories={state.repositories} avatar={state.avatar}/>
+                </div> 
+              </div>
+      )
+    }
+    const [filterParam, setParam] = useState();
+    const setFilter = (param) =>{
+      setParam(prev=>(param))
+    }
+    const [show,setShow] = useState("main")
+    const toLiked = () => {
+      setShow("liked")
+    }
+    const toMain = () => {
+      setShow("main")
+    }
+     
 
-  return (
-    <main className="App">
-      <NavBar toLiked={toLiked} setStorage={setStorage} />
-      <section class="main-container">
-        <div id="search-and-filter">
-          <Search fetchData={fetchData} />
-          <Filter setFilter={setFilter}></Filter>
-        </div>
-        <Scroll showBelow={250} />
-        {show === "main" &&
-          (state.name ? (
-            <Show />
-          ) : (
-            <div id="show-question-mark">
-              <img src={state.avatar} alt="nothing"></img>
-            </div>
-          ))}
-        {show === "liked" && <ShowLiked toMain={toMain} />}
-      </section>
-    </main>
-  );
+    return (
+      <main className="App">
+        <NavBar toLiked={toLiked} setStorage={ setStorage }/>
+        <section class="main-container">
+          <div id="search-and-filter">
+            <Search fetchData={fetchData}/>
+            <Filter setFilter={setFilter}></Filter>
+          </div>
+          
+          { show==="main" && (state.name ? <Show /> : <div id="show-question-mark"><img src={ state.avatar } alt="nothing"></img></div>)}
+          { show==="liked" && <ShowLiked toMain={toMain}/>}
+        </section>
+        
+               
+        
+      </main>
+);
 }
+            
