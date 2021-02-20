6a77fb89b77af6530db41106647bad5cfb046bf8
Michael Xiang <yorkmike4@gmail.com>
2021-02-20 01:53:49 +0000

    do not push

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -66,7 +66,7 @@ export default function Application(props) {
         <NavBar toLiked={toLiked} setStorage={ setStorage } toMain={toMain}/>
         <section class="main-container">
           <div id="search-and-filter">
-            <Search fetchData={fetchData}/>
+            <Search fetchData={fetchData} toMain={toMain}/>
             <Filter setFilter={setFilter}></Filter>
           </div>
           
